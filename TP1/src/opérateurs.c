1088d5770108a0a8656939797e93f6f6adbd9822
kaiszaitouni75 <kaiszaitouni75@gmail.com>
2026-01-29 10:24:02 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,21 @@
+#include <stdio.h>
 
+int main() {
+
+    int a = 16;
+    int b = 3;
+
+    /* Opérateurs arithmétiques */
+    printf("Addition (a + b) = %d\n", a + b);
+    printf("Soustraction (a - b) = %d\n", a - b);
+    printf("Multiplication (a * b) = %d\n", a * b);
+    printf("Division (a / b) = %d\n", a / b);
+    printf("Modulo (a %% b) = %d\n\n", a % b);
+
+    /* Opérateurs de comparaison */
+    printf("a == b : %d\n", a == b);
+    printf("a > b : %d\n", a > b);
+
+    return 0;
+}
 
