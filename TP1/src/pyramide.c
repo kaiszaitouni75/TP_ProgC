218ca1b7555fb84c9f7413023e7f39002dcb1465
kaiszaitouni75 <kaiszaitouni75@gmail.com>
2026-01-29 10:34:41 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+    int n = 5;
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+
+        /* Affichage des espaces */
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        /* Nombres croissants */
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        /* Nombres décroissants */
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        /* Passage à la ligne suivante */
+        printf("\n");
+    }
+
+    printf("Generation de la pyramide terminee.\n");
+
+    return 0;
+}
 
